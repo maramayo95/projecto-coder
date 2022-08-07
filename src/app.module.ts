0e512f11c329491460e7bd40b1afd2fdc4baec9b
Matias Aramayo <matiasaramayo@hotmail.com>
2022-08-07 19:31:39 -0300

    trabajo terminado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import 'dotenv/config'
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DB)],
   controllers: [AppController],
   providers: [AppService],
 })
